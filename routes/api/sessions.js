d560cc2a72b094aaf7d3f822cf84f1498315c883
Ildar <ildarka008@gmail.com>
2018-11-24 17:24:15 +0200

    feature(sessions): add registering and charging

--- routes/api/sessions.js
+++ routes/api/sessions.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
-const { Session, Car } = require('model');
+const { Session, Car, Parking, User } = require('model');
+const moment = require('moment');
+const getTimeOffset = require('../../utils/get-time-offset');
+const sendNotification = require('../../utils/send-notification');
 
 router.get('/', function(req, res) {
   Session.findAll({include: [{model: Car, as: 'car'}]}).then(sessions => res.json(sessions));
@@ -14,6 +17,51 @@ router.get('/:id', function(req, res) {
       res.status(404).json({ error: 'Session not found!' });
     }
   });
+});
+
+router.post('/', async (req, res) => {
+  const { numbers, parkingId = 1 } = req.body;
+  if(numbers) {
+    const numbersArray = numbers.split('/n');
+    const parking = await Parking.findById(parkingId);
+    const existingCar = await Car.findOne({
+      include: [{ model: Session, as: 'sessions' }],
+      where: {
+        number: numbersArray[0],
+      },
+    });
+
+    if(!existingCar) {
+      console.log('Car does not exist')
+      return;
+    }
+
+    const lastSession = existingCar.sessions[existingCar.sessions.length - 1];
+    const currentTime = moment(new Date()).unix();
+    if(!lastSession || (lastSession && lastSession.closedAt)) {
+      await Session.create({
+        carId: existingCar.id,
+        openedAt: currentTime,
+        parkingId,
+      });
+      sendNotification(existingCar.userId, `You have parked at ${parking.title} parking`, { type: 'park-start'})
+    } else if(lastSession && !lastSession.closedAt) {
+      const offset = getTimeOffset(lastSession.openedAt, currentTime);
+      const timeoutMinutes = 1;
+      if(offset > timeoutMinutes) {
+        const cost = (parking.price / 60) * offset;
+        const costRounded = Math.round(cost);
+        await Session.update({
+          closedAt: currentTime,
+          cost: costRounded,
+          paid: true,
+        }, { where: { id: lastSession.id }});
+        const user = await User.findById(existingCar.userId);
+        await user.update({ balance: user.balance - costRounded });
+        sendNotification(existingCar.userId, `You have been charged ${costRounded} for staying at ${parking.title}`, { type: 'park-end', costRounded})
+      }
+    }
+  }
 });
 
 module.exports = router;
