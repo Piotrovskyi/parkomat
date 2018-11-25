feeea518aba655dec2c06042a26ab6d1861c7abb
Kost9in <Kost9inbi4@mail.ru>
2018-11-25 15:32:02 +0200

    test commit 5

--- utils/set-parking-green-status.js
+++ utils/set-parking-green-status.js
@@ -1,6 +1,6 @@
 const socket = require('./socket-connection');
 
 module.exports = () => {
-  console.log('set-parking-green-status 4');
+  console.log('set-parking-green-status 5');
   socket().emit('green');
 };
