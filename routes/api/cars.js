107a5833f995fe1dd7f67a3bcacd79bb0f96e082
Kost9in <Kost9inbi4@mail.ru>
2018-11-25 14:56:31 +0200

    add test route

--- routes/api/cars.js
+++ routes/api/cars.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res) {
   Car.findAll().then(cars => res.json(cars));
 });
 
+router.get('/set-green-status', function(req, res) {
+  setParkingGreenStatus();
+});
+
 router.get('/:id', function(req, res) {
   const { id } = req.params;
   Car.findById(id).then(car => {
@@ -15,10 +19,6 @@ router.get('/:id', function(req, res) {
       res.status(404).json({ error: 'Car not found!' });
     }
   });
-});
-
-router.get('/set-green-status', function(req, res) {
-  setParkingGreenStatus();
 });
 
 module.exports = router;
